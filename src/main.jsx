b43978218798a39c1ef9ffc440d3edd5fe92ca5f
snuke159 <snuke159@gmail.com>
2025-11-08 22:41:32 +0700

    feat: add prettier config and format codebase

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
+import Clock from './Clock.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Greeting />
-  </StrictMode>,
+    {/* <Greeting /> */}
+    <Clock />
+  </StrictMode>
 )
